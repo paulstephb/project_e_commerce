0d157338a7fbea44f1b852ddde5531f51273b935
paulstephb <paul.stephane.bernier@gmail.com>
2026-07-16 13:36:08 +0200

    role editor product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/product')]
+#[Route('/editor/product')]
 final class ProductController extends AbstractController
 {
     #[Route(name: 'app_product_index', methods: ['GET'])]

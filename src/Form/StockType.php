d005b754e52b588c2bea8d482873c8aad532f1ba
paulstephb <paul.stephane.bernier@gmail.com>
2026-07-17 11:46:07 +0200

    stockcontroller + form en cours

---
+++ src/Form/StockType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\product;
+use App\Entity\Stock;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StockType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('quantity')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('product', EntityType::class, [
+                'class' => product::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Stock::class,
+        ]);
+    }
+}

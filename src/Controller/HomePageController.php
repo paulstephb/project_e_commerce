7cce9347476ec5bb45cb7ed4451c51177752d093
paulstephb <paul.stephane.bernier@gmail.com>
2026-07-22 14:27:11 +0200

    home_product_show

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Product;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,12 +12,19 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page', methods: ['GET'])]
-    public function index(ProductRepository $productRepository): Response
+    public function index(CategoryRepository $categoryRepository, ProductRepository $productRepository): Response
     {
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
             'products' => $productRepository->findAll(),
+            'categories'=>$categoryRepository->findAll(),
         ]);
+    }
+
+    #[Route('/product/{id}/show',name: 'app_home_product_show', methods: ['GET'])]
+    public function showProduct(Product $product): Response
+    {
+        return $this->render('home_page/show.html.twig', ['product'=>$product]);
     }
 
 }

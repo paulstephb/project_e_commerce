8da324219d5b1d99fbf26743d27e76fb43f64673
paulstephb <paul.stephane.bernier@gmail.com>
2026-07-24 11:46:56 +0200

    cart controller created

---
+++ src/Controller/CartController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CartController extends AbstractController
+{
+    public function __construct(private readonly ProductRepository $productRepository) {}
+
+    #[Route('/cart', name: 'app_cart', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('cart/index.html.twig', [
+            'controller_name' => 'CartController',
+        ]);
+    }
+}
